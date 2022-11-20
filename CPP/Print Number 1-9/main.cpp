343e93e55129a19cffb4e8b54f671abca6854ee2
AswinA <aswinarung2@gmail.com>
2022-11-20 13:22:42 +0700

    Finish CPP Project (Print Number 1-9)

---
+++ CPP/Print Number 1-9/main.cpp
@@ -0,0 +1,192 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // For loop angka 1
+    // for(int  i = 0; i < 6; ++i){
+    //     cout << "*" << endl;
+    // }
+
+    // For loop angka 2
+    // for(int  i = 0; i < 7; ++i){
+    //     for(int j = 0; j < 6; ++j){
+    //         if(i == 0 || i == 3 || i == 6){
+    //             cout << "*";
+    //         }else{
+    //             if(j == 5 && i <= 3){
+    //                 cout << "*";
+    //             }else if(j == 0 && i > 3){
+    //                 cout << "*";
+    //             }else{
+    //                 cout << " ";
+    //             }
+    //         }
+    //     }
+        
+    //     cout << endl;
+    // }
+
+    // For loop angka 3
+    // for(int  i = 0; i < 7; ++i){
+    //     for(int j = 0; j < 6; ++j){
+    //         if(i == 0 || i == 3 || i == 6){
+    //             cout << "*";
+    //         }else{
+    //             if(j == 5){
+    //                 cout << "*";
+    //             }else{
+    //                 cout << " ";
+    //             }
+    //         }
+    //     }
+        
+    //     cout << endl;
+    // }
+
+    // For loop angka 4
+    // for(int  i = 0; i < 7; ++i){
+    //     for(int j = 0; j < 6; ++j){
+    //         if(i < 3){
+    //             if(j == 0 || j == 5){
+    //                 cout << "*";
+    //             }else{
+    //                 cout << " ";
+    //             }
+    //         }
+            
+    //         if(i == 3){
+    //             cout << "*";
+    //         }
+            
+    //         if(i > 3){
+    //             if(j == 5){
+    //                 cout << "*";
+    //             }else{
+    //                 cout << " ";
+    //             }
+    //         }
+    //     }
+        
+    //     cout << endl;
+    // }
+
+    // For loop angka 5
+    // for(int i = 0; i < 11; ++i){
+    //     for(int j = 0; j < 9; ++j){
+    //         if(i == 0 || i == 5 || i == 10){
+    //             cout << "*";
+    //         }else{
+    //             if(i > 5){
+    //                 if(j == 8){
+    //                     cout << "*";
+    //                     break;
+    //                 }else{
+    //                     cout << " ";
+    //                 }
+    //             }else{
+    //                 cout << "*";
+    //                 break;
+    //             }
+    //         }
+    //     }
+
+    //     cout << endl;
+    // }
+
+    // For loop angka 6
+    // for(int i = 0; i < 7; ++i){
+    //     for(int j = 0; j < 8; ++j){
+    //         if(i == 1 || i == 2){
+    //             cout << "*";
+    //             break;
+    //         }else if(i == 4 || i == 5){
+    //             if(j == 0 || j == 7){
+    //                 cout << "*";
+    //             }else{
+    //                 cout << " ";
+    //             }
+    //         }else{
+    //             cout << "*";
+    //         }
+    //     }
+
+    //     cout << endl;
+    // }
+
+    // For loop angka 7
+    // for(int  i = 0; i < 7; ++i){
+    //     for(int j = 0; j < 9; ++j){
+    //         if(i == 0){
+    //             cout << "*";
+    //         }
+            
+    //         if(i > 0){
+    //             if(j == 8){
+    //                 cout << "*";
+    //             }else{
+    //                 cout << " ";
+    //             }
+    //         }
+    //     }
+        
+    //     cout << endl;
+    // }
+
+    // For loop angka 8
+    // for(int  i = 0; i < 7; ++i){
+    //     for(int j = 0; j < 9; ++j){
+    //         if(i == 0 || i == 3 || i == 6){
+    //             cout << "*";
+    //         }
+            
+    //         if(i > 0 && i < 3){
+    //             if(j == 0 || j == 8){
+    //                 cout << "*";
+    //             }else{
+    //                 cout << " ";
+    //             }
+    //         }
+            
+    //         if(i > 3 && i < 6){
+    //             if(j == 0 || j == 8){
+    //                 cout << "*";
+    //             }else{
+    //                 cout << " ";
+    //             }
+    //         }
+    //     }
+        
+    //     cout << endl;
+    // }
+
+    // For loop angka 9
+    // for(int  i = 0; i < 7; ++i){
+    //     for(int j = 0; j < 9; ++j){
+    //         if(i == 0 || i == 3 || i == 6){
+    //             cout << "*";
+    //         }
+            
+    //         if(i > 0 && i < 3){
+    //             if(j == 0 || j == 8){
+    //                 cout << "*";
+    //             }else{
+    //                 cout << " ";
+    //             }
+    //         }
+            
+    //         if(i > 3 && i < 6){
+    //             if(j == 8){
+    //                 cout << "*";
+    //             }else{
+    //                 cout << " ";
+    //             }
+    //         }
+    //     }
+        
+    //     cout << endl;
+    // }
+    
+    return 0;
+}
